d621b46ea764c4c0f9fa6f278301a3bb4644a938
b0ring <ci.i.cxcvii@gmail.com>
2024-09-04 12:48:43 +0800

    Update IITC Plugin - Player Inventory (Kaizo Version).user.js

--- IITC Plugin - Player Inventory (Kaizo Version).user.js
+++ IITC Plugin - Player Inventory (Kaizo Version).user.js
@@ -1,6 +1,6 @@
     // ==UserScript==
     // @author        jaiperdu@kai
-    // @name          IITC plugin: Player Inventory (Kaizo Version)
+    // @name          IITC plugin: Player Inventory 改
     // @category      Info
     // @version       0.4.4.001
     // @description   View inventory and highlight portals with keys at any zoom. Can be used with the official plugins Keys and Keys on map to show the number of keys on the map.
